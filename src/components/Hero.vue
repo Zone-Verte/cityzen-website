b005251ec4ec8fda6813775dfc82acbefd981cee
Khalil <khalil.chettaoui06@gmail.com>
2022-11-12 15:45:53 +0100

    hero and navbar

---
+++ src/components/Hero.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="hero min-h-screen" style="background-image: url(https://images.pexels.com/photos/192136/pexels-photo-192136.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2);">
+        <div class="hero-overlay bg-opacity-60"></div>
+        <div class="hero-content text-center text-neutral-content text-white">
+            <div class="max-w-md">
+                <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+                <p class="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
+                    exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                <button class="btn btn-primary">Get Started</button>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/components/MidSection.vue
@@ -0,0 +1,6 @@
+<template>
+    <!-- Full-width fluid until the `md` breakpoint, then lock to container -->
+    <div class="md:container md:mx-auto">
+        <!-- ... -->
+    </div>
+</template>
