791bff2ad5972d24f6c46e9dd27ae860b35cf4fd
Khalil <khalil.chettaoui06@gmail.com>
2022-11-12 17:19:30 +0100

    navbar changes

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -15,7 +15,7 @@
               <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
             </svg>
           </a>
-          <ul class="p-2 bg-base-100">
+          <ul class="p-2 bg-base-100 bg-white text-black">
             <li><a>Submenu 1</a></li>
             <li><a>Submenu 2</a></li>
           </ul>
@@ -50,5 +50,6 @@ export default defineComponent({
 .change_color {
   background-color: #f8f9fa;
   color:black;
+
 }
 </style>
