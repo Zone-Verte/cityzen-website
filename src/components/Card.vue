bb54de0ecb7d00fbbadf144e221ebce22f058055
Khalil <khalil.chettaoui06@gmail.com>
2022-11-26 17:11:22 +0100

    fonts

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,15 +1,20 @@
 <script setup lang="ts">
 </script>
 <template>
-<div class="card w-96 bg-base-100 shadow-xl image-full">
-  <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-  <div class="card-body">
-    <h2 class="card-title">Lorem ipsum!</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, animi atque velit, quaerat temporibus perferendis nobis cum necessitatibus delectus impedit eligendi aut! Consequuntur reprehenderit sunt fugit inventore ducimus, cupiditate praesentium!</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
+  <div class="card w-96 bg-base-100 shadow-xl image-full text-white">
+    <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+    <div class="card-body">
+      <h2 class="card-title text-white">Lorem ipsum!</h2>
+      <p class="text-white">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, animi
+        atque velit, quaerat temporibus perferendis nobis cum necessitatibus
+        delectus impedit eligendi aut! Consequuntur reprehenderit sunt fugit
+        inventore ducimus, cupiditate praesentium!
+      </p>
+      <div class="card-actions justify-end">
+        <button class="btn btn-primary">Buy Now</button>
+      </div>
     </div>
   </div>
-</div>
 </template>
 <style></style>
