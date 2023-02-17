2ddf469af8fa13efe343a57b00ac7fec11599023
Khalil <khalil.chettaoui06@gmail.com>
2023-02-17 16:39:46 +0100

    finelanems

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Navbar from "../components/navbar.vue";
-import Hero from "../components/hero.vue";
+import Navbar from "../components/Navbar.vue";
+import Hero from "../components/Hero.vue";
 // import MidSection from './components/MidSection.vue'
-import TextCardVue from "../components/text-card.vue";
-import FlexContainerVue from "../components/flex-container.vue";
-import CardVue from "./components/card.vue";
-import TextCardContainerVue from "../components/text-card-container.vue";
+import TextCardVue from "../components/TextCard.vue";
+import FlexContainerVue from "../components/FlexContainer.vue";
+import CardVue from "./components/Card.vue";
+import TextCardContainerVue from "../components/TextCardContainer.vue";
 import FooterVue from "../components/footer.vue";
 import FillerSection from "../components/filler-section.vue";
 </script>
