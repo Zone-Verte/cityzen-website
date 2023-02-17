e6487e156c1920fe3694fd27f3b36c0050760729
Khalil <khalil.chettaoui06@gmail.com>
2023-02-17 15:56:01 +0100

    fixing file names

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,13 +2,13 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import Navbar from "../components/navbar.vue";
-import Hero from "../components/Hero.vue";
+import Hero from "../components/hero.vue";
 // import MidSection from './components/MidSection.vue'
 import TextCardVue from "../components/text-card.vue";
 import FlexContainerVue from "../components/flex-container.vue";
-import CardVue from "./components/Card.vue";
+import CardVue from "./components/card.vue";
 import TextCardContainerVue from "../components/text-card-container.vue";
-import FooterVue from "../components/Footer.vue";
+import FooterVue from "../components/footer.vue";
 import FillerSection from "../components/filler-section.vue";
 </script>
 <template>
