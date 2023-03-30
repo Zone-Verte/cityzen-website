a8ae2a63e0dc726eca02ab9343463bdf0eebf377
Khalil <khalil.chettaoui06@gmail.com>
2023-03-30 01:25:19 +0100

    more changes to components to fit theme

---
+++ src/components/Vision.vue
@@ -0,0 +1,33 @@
+<template>
+    <div
+        class=" py-32 display flex justify-center items-center flex-col text-center gap-12 bg-[#fff] text-[#1b4965] px-20 ">
+        <h1 class="text-7xl font-[Inter] font-bold">Nos Axes Stratégique</h1>
+        <div>
+            <p>Mieux vivre ensemble de manière responsable au sein d’une communauté active pour préserver la Zone Verte et
+                engagée dans le développement durable
+            </p>
+
+        </div>
+        <div class=" grid md:grid-cols-3 justify-center items-center center content-center gap-4">
+            <div
+                class="flex justify-center py-3 rounded-lg h-60 w-96 hover:border-[#25a740] hover:border  shadow-lg cursor-pointer  items-center flex-col gap-6">
+                <img src="../assets/mission/nature.svg" alt="nature" class=" h-32 " />
+                <p>L’entretien et l’amélioration continue de la Zone Verte d’El Menzah</p>
+            </div>
+            <div
+                class="flex justify-center py-3 rounded-lg h-60 w-96 hover:border-[#25a740] hover:border shadow-lg cursor-pointer  items-center flex-col gap-6">
+
+                <img src="../assets/mission/learning.svg" alt="nature" class=" h-32 " />
+                <p>L’éducation citoyenne et environnementale</p>
+            </div>
+            <div
+                class="flex justify-center py-3 rounded-lg h-60 w-96 hover:border-[#25a740] hover:border shadow-lg cursor-pointer  items-center flex-col gap-6">
+
+                <img src="../assets/mission/partenariat.svg" alt="nature" class=" h-32 " />
+                <p>Le développement de partenariats</p>
+            </div>
+        </div>
+
+    </div>
+
+</template>
