ae8e3d3405682216f86aa5f82e89fe29377f03eb
Khalil <khalil.chettaoui06@gmail.com>
2023-03-30 19:43:17 +0100

    text content add

--- src/components/Solutions.vue
+++ src/components/Solutions.vue
@@ -8,37 +8,43 @@
             </p>
 
         </div>
-        <div class=" grid md:grid-cols-2 justify-center items-center center content-center gap-4 text-neutral-content relative">
+        <div
+            class=" grid md:grid-cols-2 justify-center items-center center content-center gap-4 text-neutral-content relative">
             <div
                 class="hidden absolute text-neutral h-[200px] w-[200px] bg-white top-[38%] right-[38%] rounded-full lg:flex justify-center items-center">
                 <!-- <h1 class="text-5xl font-bold text-primary ">CITY</h1> -->
-                <img src="../assets/LogoCityzen.svg" class="  h-40 w-40 rounded-full"/>
+                <img src="../assets/LogoCityzen.svg" class="  h-40 w-40 rounded-full" />
             </div>
             <div
-                class="h-[400px] w-[400px] lg:h-[400px] lg:w-[400px] bg-primary md:h-[300px] md:w-[300px] rounded-[100px] rounded-bl-[100px] flex justify-start pt-20 gap-8 items-center flex-col">
-                <h1 class="text-neutral-content font-bold text-5xl"><span class="bloc lg:hidden">CITY</span>  MAP</h1>
-                <p>Il s’agit d’une carte dynamique, sur laquelle les citoyens peuvent signaler leurs problèmes
-                    d’infrastructure, d’hygiène, de corruption...
+                class="h-[400px] w-[400px] lg:h-[400px] lg:w-[400px] bg-primary md:h-[300px] md:w-[300px] rounded-[100px] rounded-bl-[100px] flex justify-start pt-20 gap-8 items-center flex-col px-7 hover:cursor-pointer">
+                <h1 class="text-neutral-content font-bold text-5xl"><span class="bloc lg:hidden">CITY</span> MAP</h1>
+                <p>carte dynamique, sur laquelle les citoyens peuvent signaler leurs problèmes d’infrastructure, d’hygiène,
+                    de corruption et tout autre problème en relation avec le domaine d’interventions des pouvoirs locaux.
                 </p>
             </div>
             <div
-                class="h-[400px] w-[400px] lg:h-[400px] lg:w-[400px] bg-primary md:h-[300px] md:w-[300px]  rounded-[100px] rounded-br-[100px] flex justify-start pt-20 gap-8 items-center flex-col">
+                class="h-[400px] w-[400px] lg:h-[400px] lg:w-[400px] bg-primary md:h-[300px] md:w-[300px]  rounded-[100px] rounded-br-[100px] flex justify-start pt-20 gap-8 items-center flex-col px-7 hover:cursor-pointer">
                 <h1 class="text-neutral-content font-bold text-5xl"><span class="bloc lg:hidden">CITY</span> INVEST</h1>
-                <p>Il s’agit de suivre les projets élaborés à l’échelle locale dans leurs étapes et leurs progressions.</p>
+                <p> il s’agit de suivre les projets élaborés à l’échelle locale dans leurs étapes et leurs progressions.Il
+                    est également important de prendre en compte la continuité et la durabilité des projets élaborés à
+                    l'échelle locale en évaluant leur mise en œuvre dans le temps.</p>
             </div>
             <div
-                class="h-[400px] w-[400px] lg:h-[400px] lg:w-[400px] bg-primary md:h-[300px] md:w-[300px]  rounded-[100px] rounded-br-[100px] flex justify-start pt-20 gap-8 items-center flex-col">
-                <h1 class="text-neutral-content font-bold text-5xl"><span class="bloc lg:hidden">CITY</span>  VOTE</h1>
-                <p>Permettra aux citoyens de donner des idées et de voter sur les décisions du conseil municipal. La
-                    plateforme sera gérée par la société civile...</p>
+                class="h-[400px] w-[400px] lg:h-[400px] lg:w-[400px] bg-primary md:h-[300px] md:w-[300px]  rounded-[100px] rounded-br-[100px] flex justify-start pt-20 gap-8 items-center flex-col px-7 hover:cursor-pointer">
+                <h1 class="text-neutral-content font-bold text-5xl"><span class="bloc lg:hidden">CITY</span> VOTE</h1>
+                <p>permettra aux citoyens de donner des idées et de voter sur les décisions du conseil municipal. La
+                    plateforme sera gérée par la société civile et les résultats des votes des citoyens seront utilisés
+                    comme supports de plaidoyer pour convaincre l'assemblée d'adopter la décision la plus populaire.</p>
             </div>
             <div
-                class="h-[400px] w-[400px] lg:h-[400px] lg:w-[400px] bg-primary md:h-[300px] md:w-[300px]  rounded-[100px] rounded-bl-[100px] flex justify-start pt-20 gap-8 items-center flex-col">
-                <h1 class="text-neutral-content font-bold text-5xl"><span class="bloc lg:hidden">CITY</span>  METERS</h1>
-                <p>Mesure la performance du conseil municipale et le respect de se promet suite à une méthodologie fournie
-                </p>
-            </div>
-        </div>
+                class="h-[400px] w-[400px] lg:h-[400px] lg:w-[400px] bg-primary md:h-[300px] md:w-[300px]  rounded-[100px] rounded-bl-[100px] flex justify-start pt-20 gap-8 items-center flex-col px-7 hover:cursor-pointer">
+                <h1 class="text-neutral-content font-bold text-5xl"><span class="bloc lg:hidden">CITY</span> METERS</h1>
+                <p> mesure la performance du conseil municipale et le respect de se promet suite à une méthodologie fournie
+                    La méthodologie fournie peut inclure des indicateurs tels que la satisfaction des citoyens, l'efficacité
+                    de la gestion des fonds publics <br>
 
+            </p>
+        </div>
     </div>
-</template>
+
+</div></template>
