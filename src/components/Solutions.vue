fe7be0d2a00e51a1d70ed45504a88e98b58747e4
Khalil <khalil.chettaoui06@gmail.com>
2023-03-30 17:53:22 +0100

    treffle

--- src/components/Solutions.vue
+++ src/components/Solutions.vue
@@ -14,25 +14,25 @@
                 <h1 class="text-5xl font-bold text-primary ">CITY</h1>
             </div>
             <div
-                class="h-[400px] w-[400px] lg:h-[400px] lg:w-[400px] bg-primary md:h-[300px] md:w-[300px] rounded-tr-[100px] rounded-bl-[100px] flex justify-start pt-20 gap-8 items-center flex-col">
+                class="h-[400px] w-[400px] lg:h-[400px] lg:w-[400px] bg-primary md:h-[300px] md:w-[300px] rounded-[100px] rounded-bl-[100px] flex justify-start pt-20 gap-8 items-center flex-col">
                 <h1 class="text-neutral-content font-bold text-5xl"><span class="bloc lg:hidden">CITY</span>  MAP</h1>
                 <p>Il s’agit d’une carte dynamique, sur laquelle les citoyens peuvent signaler leurs problèmes
                     d’infrastructure, d’hygiène, de corruption...
                 </p>
             </div>
             <div
-                class="h-[400px] w-[400px] lg:h-[400px] lg:w-[400px] bg-primary md:h-[300px] md:w-[300px]  rounded-tl-[100px] rounded-br-[100px] flex justify-start pt-20 gap-8 items-center flex-col">
+                class="h-[400px] w-[400px] lg:h-[400px] lg:w-[400px] bg-primary md:h-[300px] md:w-[300px]  rounded-[100px] rounded-br-[100px] flex justify-start pt-20 gap-8 items-center flex-col">
                 <h1 class="text-neutral-content font-bold text-5xl"><span class="bloc lg:hidden">CITY</span> INVEST</h1>
                 <p>Il s’agit de suivre les projets élaborés à l’échelle locale dans leurs étapes et leurs progressions.</p>
             </div>
             <div
-                class="h-[400px] w-[400px] lg:h-[400px] lg:w-[400px] bg-primary md:h-[300px] md:w-[300px]  rounded-tl-[100px] rounded-br-[100px] flex justify-start pt-20 gap-8 items-center flex-col">
+                class="h-[400px] w-[400px] lg:h-[400px] lg:w-[400px] bg-primary md:h-[300px] md:w-[300px]  rounded-[100px] rounded-br-[100px] flex justify-start pt-20 gap-8 items-center flex-col">
                 <h1 class="text-neutral-content font-bold text-5xl"><span class="bloc lg:hidden">CITY</span>  VOTE</h1>
                 <p>Permettra aux citoyens de donner des idées et de voter sur les décisions du conseil municipal. La
                     plateforme sera gérée par la société civile...</p>
             </div>
             <div
-                class="h-[400px] w-[400px] lg:h-[400px] lg:w-[400px] bg-primary md:h-[300px] md:w-[300px]  rounded-tr-[100px] rounded-bl-[100px] flex justify-start pt-20 gap-8 items-center flex-col">
+                class="h-[400px] w-[400px] lg:h-[400px] lg:w-[400px] bg-primary md:h-[300px] md:w-[300px]  rounded-[100px] rounded-bl-[100px] flex justify-start pt-20 gap-8 items-center flex-col">
                 <h1 class="text-neutral-content font-bold text-5xl"><span class="bloc lg:hidden">CITY</span>  METERS</h1>
                 <p>Mesure la performance du conseil municipale et le respect de se promet suite à une méthodologie fournie
                 </p>
